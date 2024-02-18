e91f95bc8c0ba026066673cb7b6761132f16c6ad
EthanDorr <egdorr@gmail.com>
2024-02-18 00:02:37 -0500

    added app icon and some global mercer colors

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 final GlobalKey<ScaffoldMessengerState> scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
+const Color mercerOrange = Color.fromARGB(255, 247, 104, 000);
+const Color mercerBlack = Color.fromARGB(255, 34, 34, 34);
+const Color mercerDarkGray = Color.fromARGB(255, 63, 63, 65);
--- lib/map.dart
+++ lib/map.dart
@@ -7,7 +7,8 @@ import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 import 'package:bear_tracks/location.dart';
-  
+import 'package:bear_tracks/globals.dart';
+
 class MapScreen extends StatefulWidget { 
   const MapScreen({super.key}); 
   
@@ -40,46 +41,48 @@ class _MapScreenState extends State<MapScreen> {
     }
   }
 
-  /*
-  Future<LatLng> _getInitialLatLng() async {
-    Position? initialPosition = await geolocator.getCurrentPosition();
-    return initialPosition != null
-      ? LatLng(initialPosition.latitude, initialPosition.longitude)
-      : const LatLng(32.827, -83.648); // Default starting coordinate. Should point to the approximate center of campus.
-  }
-  */
-
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder<LatLng>(
         future: _getInitialLatLng(),
         builder: (context, snapshot) {
+          // While the app is loading, display a loading indicator
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
+            // TODO: Abstract into loading screen
+            return const Center(child: CircularProgressIndicator(
+                color: mercerOrange,
+                backgroundColor: mercerDarkGray,
+            ));
+          // TODO: Maybe don't display debug output in production
+          // If there was an error, present it (for now)
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          // If there was no data (somehow?), present it (for now)
+          } else if (!snapshot.hasData) {
+            return const Center(child: Text('No data'));
           }
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
+
           LatLng initialLatLng = snapshot.data!;
 
           return FlutterMap( 
             mapController: controller, 
             options: MapOptions(
               initialCenter: initialLatLng, 
+              minZoom: 16,
+              maxZoom: 19,
               initialZoom: 18,
             ), 
             children: [
               TileLayer(
                 urlTemplate: const String.fromEnvironment('PUBLIC_API_KEY'),
-                minZoom: 16,
-                maxZoom: 19,
               ),
               CurrentLocationLayer(),
             ],
           );
         },
       ),
+      backgroundColor: Colors.black,
     );
   }
 } 
