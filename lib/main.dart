13bb4bc6be19724216147f6ba6e1bc9a60f96271
EthanDorr <egdorr@gmail.com>
2024-02-23 00:16:51 -0500

    Wow, this is a lot. Finished the GPS. Finalized app icon (for now) Fixed app name Added map caching (woohoo!) Added OpenStreetMaps credits

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
-import 'package:flutter/material.dart';
- 
 import 'package:bear_tracks/globals.dart';
 import 'package:bear_tracks/map.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() => runApp(const MyApp()); 
+void main() async {
+  await initHive();
+  runApp(const BearTracks());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<void> initHive() async {
+  await Hive.initFlutter();
+  await Hive.openBox('lastKnownLocation');
+}
+
+class BearTracks extends StatelessWidget {
+  const BearTracks({super.key});
   
   @override 
   Widget build(BuildContext context) {
+    // Hide the status bar
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive, overlays: [SystemUiOverlay.bottom]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
     return MaterialApp(
-      title: "Bear Tracks", 
-      debugShowCheckedModeBanner: false, 
+      title: 'Bear Tracks', 
       home: const MapScreen(),
       scaffoldMessengerKey: scaffoldKey,
     ); 
--- lib/map.dart
+++ lib/map.dart
@@ -1,88 +1,141 @@
-import 'package:flutter/foundation.dart';
+// ignore: unused_import
+import 'dart:developer';
+
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter/material.dart';
-
-import 'package:flutter_map/flutter_map.dart'; 
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:flutter_map_cache/flutter_map_cache.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import 'package:bear_tracks/location.dart';
 import 'package:bear_tracks/globals.dart';
+import 'package:bear_tracks/gps.dart';
 
-class MapScreen extends StatefulWidget { 
+
+
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key}); 
-  
-  @override 
-  State<MapScreen> createState() => _MapScreenState(); 
-} 
-  
-class _MapScreenState extends State<MapScreen> { 
-  final MapController controller = MapController();
-  GeolocationService geolocator = GeolocationService();
-  late Future<LatLng> initialLatLng;
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> with TickerProviderStateMixin {
+  bool isLocationEnabled = false;
+  LatLng initialLatLng = mercerLatLng;
+  final AlignOnUpdate _alignPositionOnUpdate = AlignOnUpdate.never;
+  late final AnimatedMapController controller;
+
+  GPS gps = GPS();
 
   @override
   void initState() {
     super.initState();
-    initialLatLng = _getInitialLatLng();
+    _initializeGPS();
+    _initializeMapController();
   }
 
-  Future<LatLng> _getInitialLatLng() async {
-    try {
-      Position? initialPosition = await geolocator.getCurrentPosition();
-      return initialPosition != null
-          ? LatLng(initialPosition.latitude, initialPosition.longitude)
-          : const LatLng(32.827, -83.648); // Default starting coordinate.
-    } catch (e) {
-      if (kDebugMode) {
-        print("Error getting initial position: $e");
-      }
-      return const LatLng(32.827, -83.648); // Default starting coordinate on error.
-    }
+  void _initializeGPS() {
+    isLocationEnabled = gps.evaluateServiceStatus(gps.serviceStatus);
+    initialLatLng = gps.getInitialLatLng();
+
+    gps.serviceStatusStream?.listen((status) {
+      setState(() {
+        isLocationEnabled = gps.evaluateServiceStatus(status);
+      });
+    });
   }
 
-  @override 
+  void _initializeMapController() {
+    controller = AnimatedMapController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    gps.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const factory = LocationMarkerDataStreamFactory();
     return Scaffold(
-      body: FutureBuilder<LatLng>(
-        future: _getInitialLatLng(),
-        builder: (context, snapshot) {
-          // While the app is loading, display a loading indicator
+      body: FutureBuilder(
+        future: Future.wait([gps.isLocationServicesEnabledGL(), getPath()]),
+        //initialData: gps.serviceStatus,
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            // TODO: Abstract into loading screen
-            return const Center(child: CircularProgressIndicator(
+            return const Center(
+              child: CircularProgressIndicator(
                 color: mercerOrange,
                 backgroundColor: mercerDarkGray,
-            ));
-          // TODO: Maybe don't display debug output in production
-          // If there was an error, present it (for now)
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          // If there was no data (somehow?), present it (for now)
-          } else if (!snapshot.hasData) {
-            return const Center(child: Text('No data'));
+              ),
+            );
           }
+          isLocationEnabled = snapshot.data![0];
 
-          LatLng initialLatLng = snapshot.data!;
-
-          return FlutterMap( 
-            mapController: controller, 
+          return FlutterMap(
+            mapController: controller.mapController,
             options: MapOptions(
-              initialCenter: initialLatLng, 
-              minZoom: 16,
-              maxZoom: 19,
-              initialZoom: 18,
-            ), 
+              initialCenter: initialLatLng,
+              initialZoom: 18.0,
+              minZoom: 16.0,
+              maxZoom: 19.0,
+            ),
             children: [
               TileLayer(
                 urlTemplate: const String.fromEnvironment('PUBLIC_API_KEY'),
+                tileProvider: CachedTileProvider(
+                  maxStale: const Duration(days: 2),
+                  store: HiveCacheStore(
+                    snapshot.data![1],
+                    hiveBoxName: 'HiveCacheStore'
+                  )
+                )
               ),
-              CurrentLocationLayer(),
+              if (isLocationEnabled)
+                CurrentLocationLayer(
+                  alignPositionOnUpdate: _alignPositionOnUpdate,
+                  positionStream: factory.fromGeolocatorPositionStream(
+                      stream: gps.positionStream,
+                  ),
+                ),
+              RichAttributionWidget(
+                attributions: [
+                  TextSourceAttribution(
+                    'OpenStreetMap contributors',
+                    onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+                  ),
+                ],
+                alignment: AttributionAlignment.bottomLeft,
+              ),
             ],
           );
         },
       ),
-      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => recenterUserOnMap(),
+        child: isLocationEnabled? const Icon(Icons.location_searching) : const Icon(Icons.location_disabled)   
+      ),
     );
   }
-} 
+
+  Future<String> getPath() async {
+    final cacheDirectory = await getTemporaryDirectory();
+    return cacheDirectory.path;
+  }
+
+  void recenterUserOnMap() async {
+    if (await gps.isServiceStatusAndPermissionsEnabledGL(request: true)) {
+      controller.centerOnPoint(gps.latlng ?? controller.mapController.camera.center);
+    }
+  }
+}
